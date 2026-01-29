9f8d7a91693a48e33dc1cbdf09544d1b3f5e9e4f
Nadir Perazzo <160199040+n4dirp@users.noreply.github.com>
2026-01-29 18:04:22 -0300

    Bump version to 1.0.0 in __init__.py and blender_manifest.toml

--- render_commander/__init__.py
+++ render_commander/__init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "name": "Render Commander",
     "author": "Nadir Perazzo",
     "description": "Background render launcher for Cycles & EEVEE",
-    "version": (0, 1, 0),
+    "version": (1, 0, 0),
     "blender": (4, 2, 0),
     "doc_url": "https://github.com/n4dirp/Render-Commander",
     "tracker_url": "https://github.com/n4dirp/Render-Commander",

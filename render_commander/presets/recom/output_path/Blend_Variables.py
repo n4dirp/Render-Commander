3690decbe49738ea2ca99391a4051ecc037a5fcc
Nadir Perazzo <160199040+n4dirp@users.noreply.github.com>
2026-04-23 21:22:07 -0300

    refactor: update output directory and filename handling based on Blender version

--- render_commander/presets/recom/output_path/Blend_Variables.py
+++ render_commander/presets/recom/output_path/Blend_Variables.py
@@ -2,5 +2,11 @@ import bpy
 
 settings = bpy.context.window_manager.recom_render_settings
 
-settings.override_settings.output_directory = "{blend_dir}/render/"
-settings.override_settings.output_filename = "{blend_name}"
+use_templates = bpy.app.version >= (5, 0)
+
+if use_templates:
+    settings.override_settings.output_directory = "{blend_dir}"
+    settings.override_settings.output_filename = "{blend_name}"
+else:
+    settings.override_settings.output_directory = "//"
+    settings.override_settings.output_filename = "render"

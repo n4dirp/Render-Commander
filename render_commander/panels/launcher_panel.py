d76453bac9740c2560fa3797d9f32157ddcb9472
Nadir Perazzo <160199040+n4dirp@users.noreply.github.com>
2026-04-23 21:30:58 -0300

    refactor: simplify main panel layout and remove unused render menu

--- render_commander/panels/launcher_panel.py
+++ render_commander/panels/launcher_panel.py
@@ -1,24 +1,18 @@
 # ./panels/main_panel.py
 
-import logging
-from pathlib import Path
-
 import bpy
-from bpy.types import Panel, Menu
+from bpy.types import Panel
 
-from ..utils.constants import *
+from ..utils.constants import MODE_LIST
 from ..preferences import get_addon_preferences
-from ..utils.helpers import get_render_engine
-
-log = logging.getLogger(__name__)
 
 
-def _is_render_operator_active(context):
+def _blend_filepath(context):
     settings = context.window_manager.recom_render_settings
     if settings.use_external_blend:
         return bool(settings.external_blend_file_path)
-    else:
-        return bool(bpy.data.filepath)
+
+    return bool(bpy.data.filepath)
 
 
 class RECOM_PT_main_panel(Panel):
@@ -29,35 +23,15 @@ class RECOM_PT_main_panel(Panel):
     bl_region_type = "UI"
     bl_category = "Render Commander"
 
-    @classmethod
-    def poll(cls, context):
-        prefs = get_addon_preferences(context)
-        render_engine = get_render_engine(context)
-        return prefs.cycles_setup_complete if render_engine == RE_CYCLES else True
-
     def draw(self, context):
         layout = self.layout
         settings = context.window_manager.recom_render_settings
         prefs = get_addon_preferences(context)
 
         # Launcher
-        launcher_row = layout.row(align=True)
-        render_row = launcher_row.row(align=True)
-        render_op_active = _is_render_operator_active(context)
-
-        if settings.disable_render_button:
-            render_row.enabled = False
-            render_row.operator("recom.loading_button", text="Launching", icon="SORTTIME")
-        else:
-            launcher_row.active = render_op_active
-            render_row.enabled = True
-            render_row.active = render_op_active
-
-            op_render = render_row.operator("recom.background_render", text="Render", icon="CONSOLE")
-            op_render.action_type = "RENDER"
-
-        export_row = launcher_row.row(align=True)
-        export_row.operator("recom.export_render_script", text="", icon="EXPORT")
+        export_row = layout.row(align=True)
+        export_row.active = _blend_filepath(context)
+        export_row.operator("recom.export_render_script", text="Generate Scripts", icon="EXPORT")
 
         # Mode
         col = layout.column()
@@ -72,45 +46,14 @@ class RECOM_PT_main_panel(Panel):
             list_col.prop(settings, "frame_list", text="Frame List")
 
 
-class RECOM_MT_render_commander_menu(Menu):
-    bl_label = "Render Commander"
-
-    def draw(self, context):
-        layout = self.layout
-
-        col = layout.column()
-        col.enabled = _is_render_operator_active(context)
-
-        col.operator("recom.render_image", text="Render Image", icon="CONSOLE")
-        col.operator("recom.render_animation", text="Render Animation")
-        col.separator()
-        col.operator("recom.export_image", text="Export Image Render", icon="EXPORT")
-        col.operator("recom.export_animation", text="Export Animation Render")
-
-
-def menu_render_commander_submenu(self, context):
-    layout = self.layout
-    layout.separator()
-    row = layout.row()
-    row.enabled = _is_render_operator_active(context)
-    row.menu("RECOM_MT_render_commander_menu", text="Render Commander")
-
-
-classes = (
-    RECOM_PT_main_panel,
-    RECOM_MT_render_commander_menu,
-)
+classes = (RECOM_PT_main_panel,)
 
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
 
-    bpy.types.TOPBAR_MT_render.append(menu_render_commander_submenu)
-
 
 def unregister():
-    bpy.types.TOPBAR_MT_render.remove(menu_render_commander_submenu)
-
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
